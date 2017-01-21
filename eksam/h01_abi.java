dedd3a735350e8f7093319ed2ed26d0bed11d254
indrek pruul <ipruul@itcollege.ee>
2017-01-21 17:22:29 +0200

    harjutus

--- eksam/h01_abi.java
+++ eksam/h01_abi.java
@@ -4,4 +4,9 @@ package eksam;
  * Created by HP on 21.01.2017.
  */
 public class h01_abi {
+    public String getClassName(){
+        String className = this.getClass().getSimpleName();
+        System.out.println(className);
+        return className;
+    }
 }
--- eksam/h01_main.java
+++ eksam/h01_main.java
@@ -4,4 +4,17 @@ package eksam;
  * Created by HP on 21.01.2017.
  */
 public class h01_main {
+    public static void main(String[] args) {
+
+
+        h01_abi t = new h01_abi();
+        t.getClassName();
+
+
+      // Test  = new  Test();
+       // t.getClassName();
+
+
+    }
 }
+
