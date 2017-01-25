b645676c78e2f3ee689d86dc7b097e06c66718ef
Indrek Pruul <indrek.pruul@itcollege.ee>
2017-01-25 11:31:59 +0200

    MergenStuff

---
+++ src/eksam/javaEksam-master/src/JavaFX/Ring.java
@@ -0,0 +1,6 @@
+/**
+ * Küsi kasutajalt x, y kordinaadid ja raadius ning joonista talle ring.
+ * Küsi kasutajalt sisendit kuidas soovid, pealselt, et teed seda JavaFXis.
+ */
+public class Ring {
+}
---
+++ src/eksam/javaEksam-master/src/JavaFX/Ristkulik.java
@@ -0,0 +1,6 @@
+/**
+ * Küsi kasutajalt alguskordinaadid ja laius-kõrgus ning joonista talle ristkülik.
+ * Küsi kasutajalt sisendit kuidas soovid, pealselt, et teed seda JavaFXis.
+ */
+public class Ristkulik {
+}
