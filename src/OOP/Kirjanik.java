ab1caa5808a98bc15abf077a7ceba4e65cad4974
Indrek Pruul <indrek.pruul@itcollege.ee>
2017-01-25 11:55:55 +0200

    stuff

--- src/OOP/Kirjanik.java
+++ src/OOP/Kirjanik.java
@@ -11,14 +11,14 @@ package OOP;
 public class Kirjanik {
     public static void main(String[] args) {
         int tindiKogus = 60;
-//
-//        Pastakas pastakas = new Pastakas(tindiKogus);
-//
-//        pastakas.kirjuta("Elu oleks palju lihtsam, kui meil oleks selle lähtekood.");
-//
-//        pastakas.prindiPaljuTintiAlles();
-//
-//        pastakas.kirjuta("Õiged progejad ei kommenteeri oma koodi. Kui seda oli raske kirjutada, siis peab olema seda ka raske lugeda!");
+
+        Pastakas pastakas = new Pastakas(tindiKogus);
+
+        pastakas.kirjuta("Elu oleks palju lihtsam, kui meil oleks selle lähtekood.");
+
+        pastakas.prindiPaljuTintiAlles();
+
+        pastakas.kirjuta("Õiged progejad ei kommenteeri oma koodi. Kui seda oli raske kirjutada, siis peab olema seda ka raske lugeda!");
 
     }
 }
--- src/OOP/Pidu.java
+++ src/OOP/Pidu.java
@@ -13,30 +13,30 @@ public class Pidu {
     public static void main(String[] args) {
 
         int mahutab = 10;
-//        Korter korter = new Korter(mahutab);
-//
-//        korter.saabus("Taavi");
-//        korter.saabus("Pilve");
-//        korter.saabus("Maarika");
-//        korter.saabus("Joonas");
-//        korter.saabus("Kalle");
-//        korter.saabus("Muri");
-//        korter.saabus("Sille");
-//
-//        korter.prindiKylalisteArv();
-//        korter.prindiPaljuVeelMahub();
-//
-//        korter.lahkus("Taavi");
-//
-//        korter.saabus("Tambet");
-//        korter.saabus("Liisa");
-//        korter.saabus("Liis");
-//        korter.saabus("Veidro");
-//        korter.saabus("Moonika");
-//        korter.saabus("Politsei");
-//
-//        korter.lahkus("Sille");
-//
-//        korter.prindiKylalisteArv(); // peaks olema 9, järjekorda ei ole.
+        Korter korter = new Korter(mahutab);
+
+        korter.saabus("Taavi");
+        korter.saabus("Pilve");
+        korter.saabus("Maarika");
+        korter.saabus("Joonas");
+        korter.saabus("Kalle");
+        korter.saabus("Muri");
+        korter.saabus("Sille");
+
+        korter.prindiKylalisteArv();
+        korter.prindiPaljuVeelMahub();
+
+        korter.lahkus("Taavi");
+
+        korter.saabus("Tambet");
+        korter.saabus("Liisa");
+        korter.saabus("Liis");
+        korter.saabus("Veidro");
+        korter.saabus("Moonika");
+        korter.saabus("Politsei");
+
+        korter.lahkus("Sille");
+
+        korter.prindiKylalisteArv(); // peaks olema 9, järjekorda ei ole.
     }
 }
