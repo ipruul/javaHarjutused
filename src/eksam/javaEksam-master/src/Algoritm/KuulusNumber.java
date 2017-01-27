357357ba7ba057fcea696638e3f47776915e868c
indrek pruul <ipruul@itcollege.ee>
2017-01-27 14:44:56 +0200

    merge

--- src/eksam/javaEksam-master/src/Algoritm/KuulusNumber.java
+++ src/eksam/javaEksam-master/src/Algoritm/KuulusNumber.java
@@ -1,5 +1,5 @@
-import java.awt.*;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Antud on int[] massiiv. Eemalda kõik nr 3 väärtused ja leia millist numbrit esineb kõige siis rohkem?
@@ -11,14 +11,20 @@ public class KuulusNumber {
 
         int[] naide = {1, 5, 3, 3, 6, 3, 7, 7}; // vastus on 7
         for (int i = 0; i <naide.length ; i++) {
-            System.out.println(naide[i]);
-            if (naide[i] == 3){
+            // System.out.println(naide[i]);
+            if (naide[i] == 3) {
                 System.out.println("kolm");
-                Arrays.asList(naide[i]);
-                System.out.println(naide[i]);
+
+
+
+
 
 
             }
+
+
+
+
 //            int[] naide2 = {naide[i]};
 //            System.out.println(naide[i]);
 
