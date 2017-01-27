d2df8fe56ba8e9c76ce8dbb73f864c23c8645018
Indrek Pruul <indrek.pruul@itcollege.ee>
2017-01-27 12:44:55 +0200

    algo´itm

--- src/eksam/javaEksam-master/src/Algoritm/KuulusNumber.java
+++ src/eksam/javaEksam-master/src/Algoritm/KuulusNumber.java
@@ -1,5 +1,6 @@
 import java.awt.*;
-import java.util.Arrays;
+import java.util.*;
+import java.util.List;
 
 /**
  * Antud on int[] massiiv. Eemalda kõik nr 3 väärtused ja leia millist numbrit esineb kõige siis rohkem?
@@ -11,16 +12,20 @@ public class KuulusNumber {
 
         int[] naide = {1, 5, 3, 3, 6, 3, 7, 7}; // vastus on 7
         for (int i = 0; i <naide.length ; i++) {
-            System.out.println(naide[i]);
-            if (naide[i] == 3){
-                System.out.println("kolm");
-                Arrays.asList(naide[i]);
+//            System.out.println(naide[i]);
+            if (naide[i] == 1 || naide[i] == 2 || naide[i] == 4 || naide[i] == 5 || naide[i] == 6 || naide[i] == 7 ){
+//                System.out.println("kolm");
+
+
+
                 System.out.println(naide[i]);
 
 
+
+
+
             }
-//            int[] naide2 = {naide[i]};
-//            System.out.println(naide[i]);
+       ;
 
         }
     }
