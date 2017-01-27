b4ba4fcd533c24f77a8f50f4decb89822269af9c
indrek pruul <ipruul@itcollege.ee>
2017-01-27 15:20:32 +0200

    merge2

--- src/eksam/javaEksam-master/src/Algoritm/KuulusNumber.java
+++ src/eksam/javaEksam-master/src/Algoritm/KuulusNumber.java
@@ -14,37 +14,24 @@ public class KuulusNumber {
     public static void main(String[] args) {
 
         int[] naide = {1, 5, 3, 3, 6, 3, 7, 7}; // vastus on 7
-        for (int i = 0; i <naide.length ; i++) {
-<<<<<<< HEAD
+        for (int i = 0; i < naide.length; i++) {
+
             // System.out.println(naide[i]);
-            if (naide[i] == 3) {
-                System.out.println("kolm");
-
-
-
 
 //            System.out.println(naide[i]);
-            if (naide[i] == 1 || naide[i] == 2 || naide[i] == 4 || naide[i] == 5 || naide[i] == 6 || naide[i] == 7 ){
-//                System.out.println("kolm");
+                if (naide[i] == 1 || naide[i] == 2 || naide[i] == 4 || naide[i] == 5 || naide[i] == 6 || naide[i] == 7) {
 
 
-
-                System.out.println(naide[i]);
-
+                    System.out.println(naide[i]);
 
 
-
-
-            }
-
-
-
+                }
 
 
 //            int[] naide2 = {naide[i]};
 //            System.out.println(naide[i]);
 
+            }
         }
-    }
 
-}
+    }
