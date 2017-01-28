4e799235107c4895d42ab58925d541b301b1b636
Indrek Pruul <indrek.pruul@itcollege.ee>
2017-01-28 15:42:11 +0200

    kikilips

--- src/eksam/javaEksam-master/src/Maatriks/Kikilips.java
+++ src/eksam/javaEksam-master/src/Maatriks/Kikilips.java
@@ -18,11 +18,34 @@ import java.util.Arrays;
  */
 public class Kikilips {
 
-    // Lihtsalt abiline meetod, et maatriksit välja printida
-    private static void printMaatriks(int[][] laud) {
-        for (int i = 0; i < laud.length; i++) {
-            System.out.println(Arrays.toString(laud[i]));
+
+    public static void main(String[] args) {
+        System.out.println("hrllo");
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print(i);
+                System.out.print(" ");
+            }
+            System.out.println(" ");
         }
-        System.out.println("");
+        System.out.println();
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print(j);
+                System.out.print(" ");
+            }
+            System.out.println(" ");
+        }
+
+
+////     Lihtsalt abiline meetod, et maatriksit välja printida
+//    private static void printMaatriks(int[][] laud) {
+//        for (int i = 0; i < laud.length; i++) {
+//            System.out.println(Arrays.toString(laud[i]));
+//        }
+//        System.out.println("hello");
+//    }
     }
 }
