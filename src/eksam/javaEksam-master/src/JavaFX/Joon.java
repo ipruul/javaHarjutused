df8f05119c9d56f5aeaceac7c30839e05caaed61
indrek pruul <ipruul@itcollege.ee>
2017-01-28 18:50:26 +0200

    joon

--- src/eksam/javaEksam-master/src/JavaFX/Joon.java
+++ src/eksam/javaEksam-master/src/JavaFX/Joon.java
@@ -1,7 +1,34 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.stage.Stage;
+
 /**
  * Küsi kasutajalt alguse ja lõpu x-y kordinaadid ning joonista talle joon.
  * Akna võime limiteerida 500x500 piksli peale.
  * Küsi kasutajalt sisendit kuidas soovid, pealselt, et teed seda JavaFXis.
  */
-public class Joon {
+
+
+public class Joon extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Pane pane = new Pane();
+        Scene scene = new Scene(pane, 500, 500);
+
+        primaryStage.setScene(scene);
+
+
+        Circle ring = new Circle(30);
+        pane.getChildren().addAll(ring);
+
+        Line joon = new Line(100, 400, 100, 100);
+        pane.getChildren().addAll(joon);
+
+        primaryStage.show();
+    }
 }
