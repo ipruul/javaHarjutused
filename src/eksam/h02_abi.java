e8a6d93a434551457100f1b61addd4bc6e3ee8a3
indrek pruul <ipruul@itcollege.ee>
2017-01-29 12:52:34 +0200

    fx

---
+++ src/eksam/h02_abi.java
@@ -0,0 +1,7 @@
+package eksam;
+
+/**
+ * Created by ipruu_000 on 29.01.2017.
+ */
+public class h02_abi {
+}
---
+++ src/eksam/h02_main.java
@@ -0,0 +1,46 @@
+package eksam;
+
+import javafx.application.Application;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+import java.util.Collection;
+
+/**
+ * Created by ipruu_000 on 29.01.2017.
+ */
+public class h02_main extends Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Pane pane = new Pane();
+        Scene scene = new Scene(pane, 600, 500);
+
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        Circle ring = new Circle(49);
+
+        ring.setLayoutX(100);
+        ring.setLayoutY(100);
+
+        klass Kl = new klass();
+        Kl.meetod();
+
+        pane.getChildren().addAll(ring);
+        
+
+
+    }
+
+    public class klass{
+        public void meetod(){
+            System.out.println("Tere hommikust");
+        }
+
+    }
+}
